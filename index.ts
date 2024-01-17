f0bf41cb8fe68d5122dbd856478847166cc4792d
Brenopms <brenosilva1@gmail.com>
2024-01-17 18:13:13 -0300

    feat: create video from edited image and song

--- index.ts
+++ index.ts
@@ -1,7 +1,27 @@
 import { getExchangeRate } from "./exchangeRate/exchange-rate";
+import { getNormalizedRandomFileName } from "./helpers/get-normalized-random-file-name";
+import { generateImage } from "./imageLoader/image-loader";
+import { createVideo } from "./videoLoader/video-loader";
 
 export const FROM_CURRENCY = "USD" as const;
 export const TO_CURRENCY = "BRL" as const;
 
 const exchangeRate = await getExchangeRate();
-console.log(exchangeRate);
+
+const timeString = new Date().toISOString();
+const outputImagePath = `output/50-cent${timeString}-thumb.png`;
+const outputVideoPath = `output/50-cent${timeString}.mp4`;
+
+const songName = await getNormalizedRandomFileName("./songs");
+const songPath = `./songs/${songName}`;
+
+const editedImage = await generateImage(
+  (exchangeRate.bid / 2)?.toFixed(2), // 50 Cent MAGIC!
+  "./images"
+);
+
+// Write image to be used in the video
+await Bun.write(outputImagePath, editedImage);
+
+// Write Video with the created image and selected song
+await createVideo(outputVideoPath, outputImagePath, songPath);
